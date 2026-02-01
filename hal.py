cb4ee3ff2d81996eb07803491072a66718085053
anapremovic <apa109@sfu.ca>
2026-02-01 05:31:05 -0800

    Add HAL speaking to main loop

--- hal.py
+++ hal.py
@@ -2,6 +2,9 @@ from realtime_speech_to_text import SpeechToText
 import time
 from NLtoSANParser import parse
 from chess_game import ChessGame
+from SANtoVoiceLine import DicttoVoiceLine
+import text_to_speech
+import subprocess
 
 
 def run_hall():
@@ -16,6 +19,12 @@ def run_hall():
         
         dictionaries = chess_engine.handle_user_move(user_move_san)
         print(dictionaries)
+
+        if isinstance(dictionaries, dict):
+            halLine = DicttoVoiceLine(dictionaries)
+            print("hal says:\n", halLine)
+            text_to_speech.speak_text(halLine, "HAL_speech_output.mp3")
+            subprocess.run(['mpg123', 'HAL_speech_output.mp3'])
 
     stt = SpeechToText(handle_text_event=process_one_turn)
     stop_listening = stt.start_background_listening()
