8d8ca5225921028b2b6fa63209e06a5e0b2ca643
anapremovic <apa109@sfu.ca>
2026-01-31 16:09:14 -0800

    Add UI using static board SVG image and Flask

--- chess_game.py
+++ chess_game.py
@@ -1,13 +1,45 @@
 import chess
+import chess.svg
 from stockfish import Stockfish
+import os
+import threading
+from flask import Flask
+import logging
+from time import sleep
 
 TERMINATION_FLAG: str = "DONE"
 STOCKFISH_EXE_PATH: str = "stockfish-windows-x86-64-avx2.exe"
+chess_web_app = Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+@chess_web_app.route('/')
+def index():
+    """Update UI every 500 milliseconds"""
+    return """
+    <html>
+        <body style="background: #222; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+            <img id="board" src="/static/board.svg" style="height: 90vh;">
+            <script>
+                setInterval(() => {
+                    const img = document.getElementById('board');
+                    img.src = '/static/board.svg?v=' + new Date().getTime();
+                }, 500);
+            </script>
+        </body>
+    </html>
+    """
+
+def run_web_app():
+    chess_web_app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
 
 class ChessGame:
     def __init__(self) -> None:
         self.board = chess.Board()
         self.stockfish = Stockfish(path=STOCKFISH_EXE_PATH)
+        if not os.path.exists('static'):
+            os.makedirs('static')
+        self.update_ui()
 
     def play_chess(self) -> None:
         """Test function for running chess inside this module"""
@@ -24,6 +56,9 @@ class ChessGame:
         try:
             played_move = self.board.push_uci(move)
             print(f"You played {played_move}")
+
+            self.update_ui()
+            sleep(1) # stall before Stockfish turn
         except ValueError:
             print(f"{move} is invalid")
             return ""
@@ -34,6 +69,7 @@ class ChessGame:
 
         stockfish_move = self.board.push_uci(self.get_stockfish_move())
         print(f"Stockfish played {stockfish_move}")
+        self.update_ui()
 
         if self.board.is_game_over():
             self.game_over()
@@ -50,7 +86,16 @@ class ChessGame:
         print(f"Result: {self.board.result()}")
         print(f"Outcome: {self.board.outcome().termination}")
 
+    def update_ui(self):
+        last_move = self.board.peek() if self.board.move_stack else None
+        board_svg = chess.svg.board(self.board, size=800, lastmove=last_move)
+        with open("static/board.svg", "w") as f:
+            f.write(board_svg)
+
 
 if __name__ == "__main__":
+    ui_thread = threading.Thread(target=run_web_app, daemon=True)
+    ui_thread.start()
+
     chess_game = ChessGame()
     chess_game.play_chess()
